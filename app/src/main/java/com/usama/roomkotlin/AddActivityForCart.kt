21a2ad658f38f9f10654d6063687ddf2814fc255
imusama14 <m.usama@dtechsystems.co>
2021-07-18 12:25:59 +0500

    AddToCart

---
+++ app/src/main/java/com/usama/roomkotlin/AddActivityForCart.kt
@@ -0,0 +1,11 @@
+package com.usama.roomkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddActivityForCart : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_for_cart)
+    }
+}
